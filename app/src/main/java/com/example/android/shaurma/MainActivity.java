181d078774ccb6386ad44247d96e192047abf3e5
DmitriyFialkovskiy <dmitriy.fialkovskiy@gmail.com>
2018-02-28 12:50:25 +0300

    ini commit

---
+++ app/src/main/java/com/example/android/shaurma/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.android.shaurma;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        Drawable mIcon= ContextCompat.getDrawable(this, R.drawable.roll2);
+//        mIcon.mutate().setColorFilter(0xFF007F66, PorterDuff.Mode.MULTIPLY);
+//
+//        ImageView mImageView  = (ImageView) findViewById(R.id.roll);
+//
+//        mImageView.setImageDrawable(mIcon);
+    }
+
+
+}
